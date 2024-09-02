f4eba88cf68b789476bc41f2bfa939e67368c23a
temic137 <68796483+temic137@users.noreply.github.com>
2024-09-02 23:24:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ def create_app():
     
     return app
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app = create_app()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=port)
